ab3702a80378bebd998e3c43f55c770b19a05e3f
maxime7770 <maxwolf34@gmail.com>
2024-02-26 11:27:45 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prescribing Diabetes Self-Management Classes
+
+This project was developed as part of the Machine Learning Under an Optimization Lens class at MIT. It is a collaborative effort by team members [Maxime Wolf](https://www.linkedin.com/in/maxime-wolf/) and [Dilan SriDaran](https://www.linkedin.com/in/dilansridaran/)
---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+# title of the app  
+st.title('Prescribing diabetes self-management classes')
+
+st.header('Optimal Policy Trees')
+
+st.info('The parameter lambda allows you to control the trade-off between physical health and mental health of the patient. lambda = 0 means that the algorithm will only consider physical health, while lambda = 1 means that the algorithm will only consider mental health.')
+
+# slider bar
+x = st.slider('Lambda', min_value=0.0, max_value=1.0, value=0.4, step=0.2)
+
+file = "results/tree1205_lambda="+str(x)
+open(file, 'r').read()
+st.components.v1.html(open(file, 'r').read(), height=600, width=1500)
+
+
+
+st.header('Questionnaire')
+
+
+st.components.v1.html(open("results/questions_lambda="+str(x), 'r').read(), height=500, width=1000)
