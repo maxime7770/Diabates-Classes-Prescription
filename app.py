1c23c8c2493aee5c41a8489e3dd5c5610d4ec137
Maxime Wolf <58089609+maxime7770@users.noreply.github.com>
2024-05-01 15:30:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
-
+st.set_page_config(
+    layout="wide",
+    theme={"base": "light"}
+)
 # title of the app  
 st.title('Prescribing diabetes self-management classes')
 
